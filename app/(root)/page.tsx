2bb1a7d8d274d7aae1ffc32aa19544130dd71188
Labeeb Nixxie <labeeb@nixxieinternational.com>
2024-06-08 18:20:23 +0400

    trending podcast card created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
 import { Button } from '@/components/ui/button'
+import { podcastData } from '@/constants'
+import PodcastDetails from './podcast/[podcastId]/page'
+import PodcastCard from '@/components/PodcastCard'
 
 const Home = () => {
 
@@ -6,6 +9,19 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+
+        <div className='podcast_grid'>
+          {
+            podcastData?.map(({ id, title, description, imgURL }) => (
+              <PodcastCard key={id}
+                imgUrl={imgURL}
+                title={title}
+                description={description}
+                podcastId={id}
+              />
+            ))
+          }
+        </div>
       </section>
     </div>
   )

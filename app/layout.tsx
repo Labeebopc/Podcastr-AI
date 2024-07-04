200c47ab1b76b3d73159803f128d79f2b1e0415a
Labeeb OPC <Labeebopc313@gmail.com>
2024-07-04 19:11:45 +0400

    convex implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ConvexClientProvider } from "./providers/ConvexProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Podcastr",
   description: "Generate your podcast using AI",
-  icons:{
+  icons: {
     icon: '/icons/logo.svg'
   }
 };
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          {children}
+        </ConvexClientProvider></body>
     </html>
   );
 }
